3848b40d0b8d24beeda610d0d4f1a0186cd422e7
Dezilok <Dezilok@gmail.com>
2020-08-10 04:57:54 +0300

    installed pillow for image management added login, logout profile pages added model Profile for model User added signal for auto create profile after create or save changes for User. some changes in settings.py for correct media working

--- readme.txt
+++ readme.txt
@@ -1,11 +1,12 @@
 Веб блог написан на Django и Bootstrap.
 
-Package    Version
----------- -------
-asgiref    3.2.10
-Django     3.1
-pip        20.2.1
-pytz       2020.1
-setuptools 49.2.1
-sqlparse   0.3.1
+Package             Version
+------------------- -------
+asgiref             3.2.10
+Django              3.1
 django-crispy-forms 1.9.2
+Pillow              7.2.0
+pip                 20.2.1
+pytz                2020.1
+setuptools          49.2.1
+sqlparse            0.3.1
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveSmallIntegerField(default=0, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    about_me = models.TextField(default='About me')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
