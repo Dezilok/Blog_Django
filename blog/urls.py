770f071bf8d202c79907a62c4ec748a3ab658f9c
Dezilok <Dezilok@gmail.com>
2020-08-11 06:22:29 +0300

    Added logic for Users. Now you can change user data img, email, name etc. Added logic for frontend Users. Create, update, delete post logic. Added page for posts.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 
 urlpatterns = [
-    path('', views.homepage, name='blog-homepage'),
+    path('', PostListView.as_view(), name='blog-homepage'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 
 ]
